e1968ec5313ca8df5cc745e86af5fcb70ae9444c
asarium <asarium@gmail.com>
2015-10-09 23:46:17 +0200

    Use ninja on linux

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -2,10 +2,9 @@
 
 set -e
 
-cmake --build travis-build --target run_tests
-
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-    cmake --build travis-build --target run_tests
+    cd travis-build
+    ninja run_tests
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
     cmake --build travis-build --target run_tests --config $CONFIGURATION | xcpretty -c
 fi
