4070a72d16503a902a20ee422a195ab1807f2f54
asarium <asarium@gmail.com>
2015-10-10 12:12:05 +0200

    Improve CI build script

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -4,7 +4,11 @@ set -e
 
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
     cd travis-build
-    ninja run_tests
+    ninja dds_tests
+    ./test/dds_tests
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
-    cmake --build travis-build --target run_tests --config $CONFIGURATION | xcpretty -c
+    cmake --build travis-build --target dds_tests --config $CONFIGURATION | xcpretty -c
+    ./travis-build/test/$CONFIGURATION/dds_tests
 fi
+
+
