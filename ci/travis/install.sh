2775bc9424d5fa76a6e34ff86318f298d0eed01a
asarium <asarium@gmail.com>
2016-01-06 14:48:31 +0100

    Fix cmake download

--- ci/travis/install.sh
+++ ci/travis/install.sh
@@ -4,13 +4,13 @@ set -e
 
 FILENAME=
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-    FILENAME=cmake-3.3.1-Linux-x86_64
+    FILENAME=cmake-3.3.2-Linux-x86_64
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
-    FILENAME=cmake-3.3.1-Darwin-universal
+    FILENAME=cmake-3.3.2-Darwin-universal
     gem install xcpretty
 fi
 
 mkdir -p $HOME/cmake/
 
-wget -O /tmp/cmake.tar.gz http://www.cmake.org/files/v3.3/$FILENAME.tar.gz
+wget --no-check-certificate -O /tmp/cmake.tar.gz https://cmake.org/files/v3.3/$FILENAME.tar.gz
 tar -xzf /tmp/cmake.tar.gz -C $HOME/cmake/ --strip-components=1
